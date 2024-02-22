b9cd445bb8bb2ebf4913f5eba08c94e02b82a3d4
Garegin19 <garegin2002@icloud.com>
2024-02-22 14:51:33 +0000

    мелкие фиксы, осталось добавить актуальность статуса

--- book/serializer.py
+++ book/serializer.py
@@ -2,15 +2,14 @@ import re
 from rest_framework import serializers
 from book.models import Book, BookHistory
 
-
 class BookSerializer(serializers.ModelSerializer):
-
     def validate_title(self, value):
         if not value[0].isupper():
             raise serializers.ValidationError("Введите название с заглавной буквы!")
-        if re.search(r"[^a-zA-Zа-яА-Я0-9\s]", value):
-            raise serializers.ValidationError("Введите корректное название книги!")
-        return value
+        if self.context['request'].method == 'POST':
+            if re.search(r"[^a-zA-Zа-яА-Я0-9\s]", value):
+                raise serializers.ValidationError("Введите корректное название книги!")
+            return value
 
     def create(self, validated_data):
         validated_data["title"] += "."
@@ -19,10 +18,11 @@ class BookSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         pre_status = instance.status
-        instance.status = super().update(instance, validated_data)
+        instance.status = validated_data.get("status")
+        instance.save()
         status = validated_data.get("status")
         if status != pre_status:
-            BookHistory.objects.create(book=instance)
+            BookHistory.objects.create(book=instance, status=status)
         return instance
 
     def to_representation(self, instance):
@@ -36,7 +36,15 @@ class BookSerializer(serializers.ModelSerializer):
 
 class HistorySerializer(serializers.ModelSerializer):
     book = BookSerializer
+    title = serializers.StringRelatedField(source="book.title")
+    status = serializers.CharField(source="book.status")
+    def to_representation(self, instance):
+        res = super().to_representation(instance)
+        res["changed_at"] = instance.changed_at.strftime("%d.%m.%Y %H:%M:%S")
+        res["status"] = instance.book.status
+        return res
     class Meta:
         model = BookHistory
-        fields = ["id", "book", "changed_at"]
+        fields = ["book", "title", "changed_at", "status"]
+        many = True
 
